304c7eb1590d7f742438492ef612ba205f600344
Erick Ruh Cardozo <121986183+ErickRuhCardozo@users.noreply.github.com>
2024-06-15 17:05:15 -0300

    Adicionada funcionalidade de contar notas

--- script.js
+++ script.js
@@ -3,11 +3,17 @@
  * Date: May 1, 2022 - 3:03 PM 
  */
 
-const autoClickSwitch = document.querySelector('#autoClickSwitch');
-const accessKeyField = document.querySelector('#accessKeyField');
+const autoClickSwitch = document.getElementById('autoClickSwitch');
+const accessKeyField = document.getElementById('accessKeyField');
+const donationDateField = document.getElementById('donationDateField');
+const countDonationBtn = document.getElementById('countDonationsBtn');
+const cuponCountSpinner = document.getElementById('cuponCountSpinner');
+const donatedCuponParagraph = document.getElementById('donatedCuponParagraph');
+const donatedCuponCount = document.getElementById('donatedCuponCount');
 
 autoClickSwitch.addEventListener('change', onAutoClickSwitchChange);
 accessKeyField.addEventListener('change', onAccessKeyFieldChange);
+countDonationBtn.addEventListener('click', countDonatedCupons);
 
 function onAutoClickSwitchChange() {
   if (autoClickSwitch.checked) {
@@ -29,6 +35,24 @@ function disableAutoClick() {
   chrome.runtime.sendMessage({command: 'SET_AUTOCLICK_DISABLED'});
 }
 
+async function countDonatedCupons() {
+	donatedCuponParagraph.classList.add('d-none');
+	const selectedDate = donationDateField.valueAsDate;
+	const queryPeriod = selectedDate.toLocaleString('en-US', { month: '2-digit', year: 'numeric' });
+	selectedDate.setDate(selectedDate.getDate() + 1); // Fixing the 'day-off' problem. Gosh I Hate JS 😡
+	const brFormattedDate = selectedDate.toLocaleString('pt-BR', { day: '2-digit', month: '2-digit', year: 'numeric' });
+	console.log(brFormattedDate);
+	const url = `https://notaparana.pr.gov.br/nfprweb/RelatorioDocFiscalDoado?periodo=${queryPeriod}`;
+	cuponCountSpinner.classList.remove('d-none');
+	let response = await fetch(url);
+	response = await response.text();
+	const lines = response.split('\n');
+	const donatedCupons = lines.filter(l => l.includes(brFormattedDate)).length;
+	donatedCuponCount.innerText = donatedCupons;
+	cuponCountSpinner.classList.add('d-none');
+	donatedCuponParagraph.classList.remove('d-none');
+}
+
 addEventListener('load', function() {
   chrome.runtime.sendMessage({command: 'QUERY_AUTOCLICK_ENABLED'}, function(response) {
     autoClickSwitch.checked = response.enabled;
@@ -42,3 +66,7 @@ addEventListener('submit', function() {
   onAccessKeyFieldChange();
   chrome.tabs.reload();
 });
+
+let dateStr = (new Date()).toISOString();
+dateStr = dateStr.substring(0, dateStr.indexOf('T'));
+donationDateField.value = dateStr;
